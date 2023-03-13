269bb496ae124b39823c62ff9494c39176e46079
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-03-13 17:03:10 -0300

    change: changing about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,22 +17,27 @@ export const Services = () => {
       </Content>
       <Text>
         <Image>
-          <img src={Me} alt="" data-aos='fade-up'/>
+          <img src={Me} alt="" data-aos='fade-up' />
         </Image>
         <div data-aos='fade-up'>
           <h1>Eduardo Coelho</h1>
-            <P>
-              👋 My name is Eduardo Coelho, but you can just call me Eduardo. Pleasure!
-            </P>
-            <Span>
-              👨‍💻 For over 10 months developing and programming interfaces with JavaScript, React JS and Typescript.
-            </Span>
-            <P>
-            🎓 Graduating in Systems Analysis and Development at UNIFEOB University 
-            </P>
-            <Span>
-              Studying Systems Analysis and Development. I'm looking for my first opportunity as a developer. I consider myself a dedicated person who seeks to evolve and learn more every day. I keep studying and with focus and determination to achieve my goals!!
-            </Span>
+          <P>
+            👋 My name is Eduardo Coelho, but you can just call me Eduardo. Pleasure!
+          </P>
+          <Span>
+            👨‍💻 For over 11 months developing and programming interfaces with JavaScript, React JS and Typescript.
+          </Span>
+          <P>
+            🎓 Graduating in Systems Analysis and Development at UNIFEOB University
+          </P>
+          <Span>
+            Studying Systems Analysis and Development. I'm looking for my first opportunity as a developer. I consider myself a dedicated person who seeks to evolve and learn more every day. I keep studying and with focus and determination to achieve my goals!! <br />
+            Web developer passionate about problem solving and creating solutions
+            efficient for online business. I have already developed a consumer portal on the internet,
+            an online app for a small business, and responsive landing pages.
+            Interested in headless commerce and web application architecture and user experience
+            user.
+          </Span>
         </div>
       </Text>
     </AboutContainer>
